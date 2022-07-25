c5a9745023ea06696f8a66331d4805efe9f8fcfb
Guilherme Esgario <guilherme.esgario@gmail.com>
2022-07-25 11:57:16 -0300

    fix path

--- scripts/serving.sh
+++ scripts/serving.sh
@@ -1,2 +1,2 @@
 MLFLOW_TRACKING_URI=sqlite:///mlflow.db \
-mlflow models serve -m "models:/pytorch_simplenn_mnist/3" --env-manager=local --enable-mlserver --port 6000
+mlflow models serve -m "models:/pytorch_simplenn_mnist/1" --env-manager=local --enable-mlserver --port 6000
